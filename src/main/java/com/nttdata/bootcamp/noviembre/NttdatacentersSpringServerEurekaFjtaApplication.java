76e07e1b84709974c380e77906494b49be45b5c4
javitar02 <fjavitorrejon25@gmail.com>
2022-11-23 11:09:27 +0100

    Subido

---
+++ src/main/java/com/nttdata/bootcamp/noviembre/NttdatacentersSpringServerEurekaFjtaApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.bootcamp.noviembre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NttdatacentersSpringServerEurekaFjtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttdatacentersSpringServerEurekaFjtaApplication.class, args);
+	}
+
+}
